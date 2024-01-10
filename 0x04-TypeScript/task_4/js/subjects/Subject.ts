8f33d3dd1f2c2d9113f318ee16f7a2363fc156e7
Joshua Ajayi <kenduajayi@gmail.com>
2024-01-10 14:01:33 +0000

    Subject file

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,12 @@
+/// reference <reference path="Teacher.ts" />
+
+namespace Subjects {
+  export class Subject{
+    teacher: Teacher;
+
+    setTeacher = (teacher: Teacher) => {
+      this.teacher = teacher;
+    }
+    
+  }
+}
