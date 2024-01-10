67e7bd11f0491d417aeb3241fd5b18aec1ec5739
Joshua Ajayi <kenduajayi@gmail.com>
2024-01-10 14:04:35 +0000

    C++ file

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,19 @@
+/// reference <reference path="Teacher.ts" />
+/// reference <reference path="Subjects.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subject{
+
+    getRequirements = () => 'Here is the list of requirements for Cpp';
+
+    getAvailableTeacher = () => {
+      if (!this.teacher?.experienceTeachingC) return 'No available teacher';
+
+      return `Available Teacher: ${this.teacher}`;
+    }   
+  }
+}
